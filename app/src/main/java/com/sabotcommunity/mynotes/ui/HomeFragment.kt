3c62ccb11e228a72fab27ada79f6c8dc0e7c6c6b
Wakamoly <lucidsoftworksllc@gmail.com>
2020-09-28 13:24:02 -0400

    Initial commit

---
+++ app/src/main/java/com/sabotcommunity/mynotes/ui/HomeFragment.kt
@@ -0,0 +1,48 @@
+package com.sabotcommunity.mynotes.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.sabotcommunity.mynotes.R
+import com.sabotcommunity.mynotes.db.NoteDatabase
+import kotlinx.android.synthetic.main.fragment_add_note.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.launch
+
+class HomeFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        recycler_view_notes.setHasFixedSize(true)
+        recycler_view_notes.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+
+        launch {
+            context?.let{
+                val notes = NoteDatabase(it).getNoteDao().getAllNotes()
+                recycler_view_notes.adapter = NotesAdapter(notes)
+            }
+
+        }
+
+
+        button_add.setOnClickListener{
+
+            val action = HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+
+        }
+    }
+}
